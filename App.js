1be3637ffd8c87bfae632a253502135f47d2f8b6
ProgrammerFaateh90 <faatehnaeem53@gmail.com>
2024-04-30 21:34:43 +0500

    Home

--- App.js
+++ App.js
@@ -8,33 +8,96 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import CalorieCalculator from './src/components/calorieCalculator/calorie-calculator';
 import HomePage from './src/components/homePage/home-page';
 
+
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+
+const Tab = createMaterialBottomTabNavigator();
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+
 const App = () => {
-  
+
   const stack = createNativeStackNavigator();
 
   return (
-  <>
-    <NavigationContainer>
-    <stack.Navigator>
-    <stack.Screen name='First Page' component={LandingPage} options={{
-      title: '',
-      headerStyle: {
-      backgroundColor: 'black',
-    },
-    headerTintColor: '#fff',
-    headerTitleStyle: {
-      fontWeight: 'bold',
-    }}}/>
-    <stack.Screen name='Signup' component={SignUp}/>
-    <stack.Screen name='Login' component={Login}/>
-    <stack.Screen name='Lets get you fit' component={BmiCalculator}/>
-    <stack.Screen name='Calorie Calculator' component={CalorieCalculator}/>
-    <stack.Screen name='FIT FLOW' component={HomePage}/>
-    </stack.Navigator>
-    </NavigationContainer>
+    <>
+      <NavigationContainer>
+        <stack.Navigator>
+          <stack.Screen name='First Page' component={LandingPage} options={{
+            title: '',
+            headerStyle: {
+              backgroundColor: 'black',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            }
+          }} />
+          <stack.Screen name='Signup' component={SignUp} />
+          <stack.Screen name='Login' component={Login} />
+          <stack.Screen name='Lets get you fit' component={BmiCalculator} />
+          <stack.Screen name='Calorie Calculator' component={CalorieCalculator} />
+          <stack.Screen name='FIT FLOW' component={HomePage} />
+        </stack.Navigator>
+      </NavigationContainer>
+
+
+       {/* <NavigationContainer>
+        <Tab.Navigator
+          activeColor='#00ADB5'
+          inactiveColor='white'
+          barStyle={{ backgroundColor: '#393E46' }}
+        >
+
+          <Tab.Screen name="HomePage" component={HomePage}
+            options={{
+              tabBarLabel: 'Profile',
+              tabBarIcon: ({ color }) => (
+                <MaterialCommunityIcons name="account" color={color} size={26} />
+              ),
+              tabBarColor: 'red'
+            }} />
+
+          <Tab.Screen name="Login" component={Login}
+            options={{
+              tabBarLabel: 'Profile',
+              tabBarIcon: ({ color }) => (
+                <MaterialCommunityIcons name="camera" color={color} size={26} />
+              ),
+              tabBarColor: 'blue'
+            }}
+          />
+
+          <Tab.Screen name="Signup" component={SignUp}
+            options={{
+              tabBarLabel: 'Profile',
+              tabBarIcon: ({ color }) => (
+                <MaterialCommunityIcons name="music" color={color} size={26} />
+              ),
+              tabBarColor: 'green'
+            }}
+          />
+
+          <Tab.Screen name="Landing Page" component={LandingPage}
+            options={{
+              tabBarLabel: 'Profile',
+              tabBarIcon: ({ color }) => (
+                <MaterialCommunityIcons name="email" color={color} size={26} />
+              ),
+              tabBarColor: 'yellow'
+            }}
+          />
+
+        </Tab.Navigator>
+      </NavigationContainer > */}
+
     </>
   )
-  
+
 }
 
 export default App;
+
+
+
+
--- styles/container.js
+++ styles/container.js
@@ -2,13 +2,18 @@ import styled from "styled-components";
 // import { string } from "prop-types";
 
 export const Container = styled.View`
-width:100%;
-backgroundColor : #1F2544;
-flex:1;
+flex:0.3;
+backgroundColor : #393E46;
 alignItems:center;
+justifyContent: center; 
+marginTop:10px;
+width:350px
+
 `
 
 
+// alignItems:center;
+// flex:1;
 // < { $containerWidth: string, $containerHeight: string } >
 // width : ${props => props.$containerWidth || '300px'},
 // height : ${props => props.$containerHeight || '300px'},
