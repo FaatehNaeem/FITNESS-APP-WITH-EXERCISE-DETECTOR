0b494c5198b58fa318d741e9723fe3b9f55fcef9
ProgrammerFaateh90 <faatehnaeem53@gmail.com>
2024-05-09 22:58:23 +0500

    added appwrite among other dependencies

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
-const api_Url = process.env.EXPO_PUBLIC_API_URL;
-const api_key = process.env.EXPO_PUBLIC_API_KEY;
+const APPWRITE_ENDPOINT = process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT;
+const APPWRITE_PROJECT_ID= process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID;
 
 export const conf = {
-    api_Url,
-    api_key
+    APPWRITE_ENDPOINT,
+    APPWRITE_PROJECT_ID
 }
